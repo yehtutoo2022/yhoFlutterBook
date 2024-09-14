c57fd4e0650c4b8ae8e613573b42d07a54867301
yehtutoo2022 <127296355+yehtutoo2022@users.noreply.github.com>
2024-09-14 11:21:33 +0630

    Create flutter_fire_login.dart

---
+++ flutter_fire_login.dart
@@ -0,0 +1,63 @@
+import 'package:community_material_icon/community_material_icon.dart';
+import 'package:firebase_auth/firebase_auth.dart'
+    hide EmailAuthProvider, GoogleAuthProvider;
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import '../constants.dart';
+
+/// ! We use riverpod to watch the login state change, and rebuild screen.
+/// ! For more details see the riverpod example.
+final currentUserStreamProvider =
+    StreamProvider<User?>((ref) => FirebaseAuth.instance.authStateChanges());
+
+final currentUserProvider = StateProvider<User?>((ref) {
+  return ref.watch(currentUserStreamProvider).maybeWhen(
+        data: (user) => user,
+        orElse: () => null,
+      );
+});
+
+class FirebaseAuthUiExample extends ConsumerWidget {
+  const FirebaseAuthUiExample({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(currentUserStreamProvider).when(
+          data: (user) =>
+              user == null ? _buildLoginScreen() : _buildProfileScreen(),
+          error: (e, _) => Text(e.toString()),
+          loading: () => LinearProgressIndicator(),
+        );
+  }
+
+  Widget _buildLoginScreen() {
+    /// This SignInScreen comes from firebase_ui_auth package.
+    return SignInScreen(
+      headerBuilder: (_, __, ___) => Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: kAppIcon,
+      ),
+      // ! Currently there's no providerConfig for Anonymous sign in, so we
+      // add a btn ourselves.
+      footerBuilder: (context, _) {
+        return ElevatedButton.icon(
+          icon: Icon(CommunityMaterialIcons.incognito),
+          style: ElevatedButton.styleFrom(backgroundColor: Colors.deepOrange),
+          label: const Text('Log in anonymously'),
+          onPressed: FirebaseAuth.instance.signInAnonymously,
+        );
+      },
+    );
+  }
+
+  Widget _buildProfileScreen() {
+    return ProfileScreen(
+      children: const [
+        Text(
+            '🚀We could add more content to the profile screen via the `children` param.')
+      ],
+    );
+  }
+}
